17bd1634411fbb5b731f443c54e7f2905374bd76
SirTifix <146167317+SirTifix@users.noreply.github.com>
2024-02-27 15:44:49 +0800

    Delete customer directory

--- customer/include/js.php
+++
@@ -1,2 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://kit.fontawesome.com/9ea2f828e7.js" crossorigin="anonymous"></script>
