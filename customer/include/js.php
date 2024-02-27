e593644414969349d7dc0b0ade12eb171fdd68f4
SirTifix <146167317+SirTifix@users.noreply.github.com>
2024-02-27 15:47:32 +0800

    Add files via upload

---
+++ customer/include/js.php
@@ -0,0 +1,2 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/9ea2f828e7.js" crossorigin="anonymous"></script>
---
+++ customer/include/user-header.php
@@ -0,0 +1,26 @@
+<?php
+    require_once('./include/login-modal.php');
+?>
+<header>
+    <img class="circle" src="assets/img/circle-bg.png">
+    <img class="second-lec" src="assets/img/second-lec.png">
+    <img class="third-lec" src="assets/img/Vector2.png" alt="img">
+    <img class="fourth-lec" src="assets/img/Vector3.png" alt="img">
+    <img class="fifth-lec" src="assets/img/vet-dog.png" alt="img">
+
+    <nav class="navbar">
+        <div class="logo p-3 d-flex align-items-center">
+            <img class="logo-clinic" src="./assets/img/logo.png" alt="Logo" class="logosec">
+            
+            <div class="logo-text px-3">PAWSsible Solutions <br>
+                Veterinary Clinic</div>
+        </div>
+        <div class="navbar-links">
+            <a href="./index.php">Home</a>
+            <a href="./services.html">Services</a> 
+            <a href="./aboutus.html">About Us</a>
+            <a href="./book1.html">Booking</a>
+            <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a>
+        </div>
+    </nav>
+</header>
