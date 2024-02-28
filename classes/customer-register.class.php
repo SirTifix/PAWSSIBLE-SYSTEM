aec39e16f584967bf00898d1b3aca47448278178
cyphrus <145854178+cyphrus@users.noreply.github.com>
2024-02-28 21:35:00 +0800

    Add files via upload

---
+++ classes/customer-register.class.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once 'database.php';
+
+Class Register{
+    //attributes
+
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $password;
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    //Methods
+
+    function add(){
+        $sql = "INSERT INTO customer (firstname, lastname, email, password) VALUES 
+        (:firstname, :lastname, :email, :password);";
+
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':firstname', $this->firstname);
+        $query->bindParam(':lastname', $this->lastname);
+        $query->bindParam(':email', $this->email);
+        // Hash the password securely using password_hash
+        $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT);
+        $query->bindParam(':password', $hashedPassword);
+        
+        if($query->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }	
+    }
+
+    function is_email_exist(){
+        $sql = "SELECT * FROM customer WHERE email = :email;";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        if($query->execute()){
+            if($query->rowCount()>0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function show()
+    {
+        $sql = "SELECT * FROM customer ORDER BY lastname ASC, firstname ASC;";
+        $query = $this->db->connect()->prepare($sql);
+        $data = null;
+        if ($query->execute()) {
+            $data = $query->fetchAll();
+        }
+        return $data;
+    }
+}
+
+?>
