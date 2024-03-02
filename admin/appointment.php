ba83967b1aacf403819d424737cf687202ac31a0
PurpleMariposa <salazarkrystel405@gmail.com>
2024-03-02 18:27:41 +0800

    new
    new

---
+++ admin/appointment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $title = 'Appointment';
+    require_once('./include/admin-head.php');
+?>
+<body>
+    <?php
+        require_once('./include/admin-header.php')
+    ?>
+    <main>
+    <?php
+        require_once('./include/admin-sidepanel.php')
+    ?>
+
+
+    </main>
+
+</body>
+</html>
