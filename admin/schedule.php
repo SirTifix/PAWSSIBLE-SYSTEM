1640e9f79875a35f87890af8be0c4e491422a65e
PurpleMariposa <salazarkrystel405@gmail.com>
2024-03-02 17:10:43 +0800

    sched
    sched

---
+++ admin/schedule.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $title = 'Schedule';
+    require_once('./include/admin-head.php');
+?>
+<body>
+    <?php
+        require_once('./include/admin-header.php')
+    ?>
+    <main>
+    <?php
+        require_once('./include/admin-sidepanel.php')
+    ?>
+   
+    </main>
+</body>
+</html>
