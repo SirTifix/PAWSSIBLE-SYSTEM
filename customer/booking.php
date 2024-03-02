9542cdb4a0ea992fbe7dae4479e363a8ec43c5b7
PurpleMariposa <salazarkrystel405@gmail.com>
2024-03-02 15:54:20 +0800

    update
    update

--- customer/booking.php
+++ customer/booking.php
@@ -184,7 +184,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
-  <script src="./script/calendar.js"></script>
+  <script src="./assets/script/calendar.js"></script>
   <script>
     document.addEventListener("DOMContentLoaded", function() {
       initializeCalendar(); 
