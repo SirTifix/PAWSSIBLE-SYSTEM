b3829a47d2e016c65f7e7fdf85c44c0c9f11d8c5
PurpleMariposa <salazarkrystel405@gmail.com>
2024-03-05 09:07:29 +0800

    new
    new

--- admin/appointment.php
+++ admin/appointment.php
@@ -28,62 +28,65 @@
     </div>
 
     <div class="table-appointment">
-        <div class="search-con">
-        <input type="text" id="searchInput" class="search col-10" placeholder="Search here...">
-    </div>
+    <div class="row justify-content-end align-items-center m-0">
+        <div class="col-auto my-1">
+            <div class="search-con">
+                <input type="text" id="searchInput" class="search-input" placeholder="Search here...">
+            </div>
+        </div>
+        <div class="col-auto my-1">
+            <a href="./appointment.php" class="appointment-btn"><i class="far fa-calendar-check"></i>Appointment</a>
+        </div>
+        <div class="col-auto my-1">
+            <div class="d-flex align-items-center">
+                <a href="" class="appointment-btn"><i class="far fa-clock"></i>Pending</a>
+            </div>
+        </div>
+    </div> 
+
+
 
     <table class="table table-striped table-sm">
         <thead>
-                <tr>
-                    <th scope="col">Book No.</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Time</th>
-                    <th scope="col">Status</th>
-                    <th scope="col" width="5%">Action</th>
-                </tr>
-            </thead>
-            <tbody id="appointmentTableBody">
-        
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td class="d-flex justify-content-center align-items-center">
-                        <div class="crud-btn">
-                            <a href="" class="check-btn"><i class="fa-regular fa-circle-check m-1" aria-hidden="true"></i></a>
-                        </div>
-                        <div class="crud-btn">
-                            <a href="" class="edit-btn"><i class="fa-solid fa-pen-to-square" aria-hidden="true"></i></a>
-                        </div>
-                        <div class="crud-btn">
-                            <a href="" class="delete-btn"><i class="fa-regular fa-trash-can" aria-hidden="true"></i></a>
-                        </div>
-                        </td>
-                    </tr>
-            </tbody>
-        </table>
+            <tr>
+                <th scope="col">Book No.</th>
+                <th scope="col">Name</th>
+                <th scope="col">Date</th>
+                <th scope="col">Time</th>
+                <th scope="col">Status</th>
+                <th scope="col" width="5%">Action</th>
+            </tr>
+        </thead>
+        <tbody id="appointmentTableBody">
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="d-flex justify-content-center align-items-center">
+                    <div class="crud-btn">
+                        <a href="" class="check-btn"><i class="fa-regular fa-circle-check m-1" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="crud-btn">
+                        <a href="" class="edit-btn"><i class="fa-solid fa-pen-to-square" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="crud-btn">
+                        <a href="" class="delete-btn"><i class="fa-regular fa-trash-can" aria-hidden="true"></i></a>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
+   
     </div>
-    
    </section>
 
     </main>
     <?php
         require_once('./include/js.php')
     ?>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-$(document).ready(function(){
-    $('#searchInput').on('keyup', function(){
-        var searchText = $(this).val().toLowerCase();
-        $('#appointmentTable tbody tr').filter(function(){
-            $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
-        });
-    });
-});
-</script>
+
 </body>
 </html>
