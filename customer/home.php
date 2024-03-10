163e4612423f1c9deaf095f373189b57479debb3
PurpleMariposa <salazarkrystel405@gmail.com>
2024-03-10 22:14:28 +0800

    new
    new

--- customer/home.php
+++ customer/home.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user'] != 'customer'){
             <h2>compassionate care and expert attention.</h2>
         </div>
   
-        <button type="button" class="boton" style="background-color: #2A2F4F; color: white;">BOOK NOW</button>
+        <a href="./booking.php" type="button" class="boton" style="background-color: #2A2F4F; color: white;">BOOK NOW</a>
   
         <div class="social-icons col-2">
           <a href="https://www.facebook.com/profile.php?id=61551085774858"  title="facebook"> 
