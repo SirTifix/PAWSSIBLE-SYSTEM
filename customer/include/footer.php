18893da02156c40208958ef995accfd741c0426e
PurpleMariposa <salazarkrystel405@gmail.com>
2024-03-11 01:02:50 +0800

    footer update
    footer

--- customer/include/footer.php
+++ customer/include/footer.php
@@ -1,62 +1,63 @@
 <footer>
-    <div class="container-fluid d-flex justify-content-between">
+    <div class="con-links container-fluid row justify-content-between">
 
-        <div>
-            <h5>VISIT US</h5>
-            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.680559904266!2d122.0857516884195!3d6.928732813749569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x325042273b869a91%3A0x16517fcedb09f79c!2s998%20Pagadian%20City%20-%20Zamboanga%20City%20Rd%2C%20Zamboanga%2C%20Zamboanga%20del%20Sur!5e0!3m2!1sen!2sph!4v1709356300949!5m2!1sen!2sph" width="600" height="450" style="border:0;" 
-            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
-            <p>Briana Catapang Tower, <br>
-                MCLL Highway,<br> 
-                Guiwan, Zamboanga City.</p>
-        </div>
+    <div class="col-lg-3 col-md-6">
+        <h5 class="fw-bolder">VISIT US</h5>
+        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.680559904266!2d122.0857516884195!3d6.928732813749569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x325042273b869a91%3A0x16517fcedb09f79c!2s998%20Pagadian%20City%20-%20Zamboanga%20City%20Rd%2C%20Zamboanga%2C%20Zamboanga%20del%20Sur!5e0!3m2!1sen!2sph!4v1709356300949!5m2!1sen!2sph" width="600" height="450" style="border:0;" 
+        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+        <p>Briana Catapang Tower, <br>
+            MCLL Highway,<br> 
+            Guiwan, Zamboanga City.</p>
+    </div>
 
-        <div class="con">
-            <h5>LEGAL</h5>
-            <a href="#" >Terms & Conditions</a>
-            <a href="#" >Privacy Policy</a>
-            <a href="#" >Cookie Policy</a>
-        </div>
+    <div class="con col-lg-3 col-md-6">
+        <h5 class="fw-bolder">LEGAL</h5>
+        <a href="#" >Terms & Conditions</a>
+        <a href="#" >Privacy Policy</a>
+        <a href="#" >Cookie Policy</a>
+    </div>
 
-        <div class="con">
-            <h5>SUPPORT</h5>
-            <a href=""> FAQ</a>
-            <a href="" > Contact Us</a>
-            <a href="" > Cookie Policy</a>
+    <div class="con col-lg-3 col-md-6">
+        <h5 class="fw-bolder">SUPPORT</h5>
+        <a href=""> FAQ</a>
+        <a href="" > Contact Us</a>
+        <a href="" > Cookie Policy</a>
 
-            <h5 class="mt-2" > CALL US</h5>
-            <p > +63 947 731 2312</p>
-        </div>
-        
-        <div >
-            <h5>BUSINESS HOURS</h5>
-            <div class="con-time d-flex justify-content-center">
-                
-                <div class="con-time-2 row">
-                <div class="day col">
-                    <p>Mon-Fri <br> Sat <br> Sun</p>
-                </div>
-                
-                <div class="d-flex time col">
-                    <p>5:30PM - 9:00PM <br> 8:00AM - 5:00PM <br> 1:00PM - 5:00PM</p>
-                </div>
-                </div>
+        <h5 class="mt-2 fw-bolder" > CALL US</h5>
+        <p > +63 947 731 2312</p>
+    </div>
 
+    <div class="col-lg-3 col-md-6">
+        <h5 class="fw-bolder">BUSINESS HOURS</h5>
+        <div class="con-time d-flex justify-content-center">
+            
+            <div class="con-time-2 row">
+            <div class="day col">
+                <p>Mon-Fri <br> Sat <br> Sun</p>
             </div>
+            
+            <div class="d-flex time col">
+                <p>5:30PM - 9:00PM <br> 8:00AM - 5:00PM <br> 1:00PM - 5:00PM</p>
+            </div>
+            </div>
+
         </div>
+    </div>
 
     </div>
 
-    <div class="d-flex">
+    <div class="footer-branding d-flex row py-3 px-2">
 
-        <div>
-            <img src="./assets/img/logo.png" class="img-fluid" alt="">
-            <p>PAWSsible Solutions <br>
+        <div class="d-flex align-items-center mb-2 mb-sm-0">
+            <img src="./assets/img/logo.png" class="logo-footer-img img-fluid " alt="">
+            <p class="fw-bold ps-3">PAWSsible Solutions <br>
                 Veterinary Clinic</p>
         </div>
 
-        <div>
-            <p>© 2023 PAWSsible Solutions Veterinary Clinic. All Rights Reserved  </p>
+        <div class="d-flex justify-content-center align-items-end">
+            <p class="text-center m-0">© 2023 PAWSsible Solutions Veterinary Clinic. All Rights Reserved</p>
         </div>
 
     </div>
 </footer>
+
