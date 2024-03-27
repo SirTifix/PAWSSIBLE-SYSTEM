01cc0975db26ff86b47cfcb4b81fc79a99cd875e
SirTifix <saludoraf@gmail.com>
2024-03-27 22:33:41 +0800

    create history

---
+++ customer/include/history.php
@@ -0,0 +1,85 @@
+<div class="Rounded-container-form container rounded mt-2">
+    <div class="Pet-mail-table row">
+        <div class="Pet-mail-table col-md-12 border-right">
+            <div class="Pet-mail-table p-2 ">
+                <div class="search-container d-flex">
+                    <div class="search-wrapper d-flex align-items-center m-0">
+                        <input type="text" class="search" placeholder="search appointment.....">
+                        <i class="search-icon fas fa-search" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="white-container">
+                    <div class=" d-flex justify-content-center align-items-center">
+                        <div class="mail-btn">
+                            <a class="crud-text"><i class="fa-solid fa fa-envelope-o m-1" aria-hidden="true"></i>Pet
+                                Mail</a>
+                        </div>
+                    </div>
+
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="padding-left:5em;">Pet Name</th>
+                                <th scope="col">status</th>
+                                <th scope="col">Date</th>
+                            </tr>
+                        </thead>
+                        <tbody id="appointmentTableBody">
+                            <tr class="table-row">
+                                <td>
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" id="myCheckbox" name="myCheckbox">
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    Belgy
+                                </td>
+                                <td>Done</td>
+                                <td> <strong> Jan 6,2024</strong></td>
+                            </tr>
+                        </tbody>
+
+                        <tbody id="appointmentTableBody">
+                            <tr class="table-row">
+                                <td>
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" id="myCheckbox" name="myCheckbox">
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    Max
+                                </td>
+                                <td>Done</td>
+                                <td> <strong> March 6,2024</strong></td>
+                                
+
+                            </tr>
+                        </tbody>
+
+                        <tbody id="appointmentTableBody">
+                            <tr class="table-row">
+                                <td>
+                                    <label class="custom-checkbox">
+                                        <input type="checkbox" id="myCheckbox" name="myCheckbox">
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    Saila
+                                </td>
+                                <td>Done</td>
+                                <td> <strong> Feb 6,2024</strong></td>
+
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+
+
+</style>
