d48c03beb452c76a9d76508c79fd7c3824a8c58a
PurpleMariposa <salazarkrystel405@gmail.com>
2024-04-24 20:26:47 +0800

    update
    update

--- customer/include/home-header.php
+++ customer/include/home-header.php
@@ -5,28 +5,39 @@
     <img class="fourth-lec" src="assets/img/Vector3.png" alt="img">
     <img class="fifth-lec" src="assets/img/vet-dog.png" alt="img">
 
-    <nav class="navbar">
+    <div class="sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
         <div class="logo p-3 d-flex align-items-center">
             <img class="logo-clinic" src="assets/img/logo.png" alt="Logo" class="logosec">
-            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
             <div class="logo-text px-3">PAWSsible Solutions <br>
                 Veterinary Clinic</div>
         </div>
-        <div class="navbar-links">
-            <a href="home.php">Home</a>
-            <a href="services.php">Services</a> 
-            <a href="aboutus.php">About Us</a>
-            <a href="booking.php">Booking</a>
-            <div class="dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fa fa-bell"></i> Account
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="accountDropdown">
-                    <li><a class="dropdown-item" href="customer-profile.php">My Profile</a></li>
-                    <li><a class="dropdown-item" href="appointment.php">Appointments</a></li>
-                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                </ul>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <div class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <div class="navbar-links">
+                <a href="home.php">Home</a>
+                <a href="services.php">Services</a> 
+                <a href="aboutus.php">About Us</a>
+                <a href="booking.php">Booking</a>
+                <div class="dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-bell"></i> Account
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="accountDropdown">
+                        <li><a class="dropdown-item" href="customer-profile.php">My Profile</a></li>
+                        <li><a class="dropdown-item" href="appointment.php">Appointments</a></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
             </div>
         </div>
+        </div>
     </nav>
+    </div>
 </header>
