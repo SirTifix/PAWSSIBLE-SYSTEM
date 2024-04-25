2f312384ae4216d359c04856174cba8dee8fa2f0
PurpleMariposa <salazarkrystel405@gmail.com>
2024-04-25 17:38:49 +0800

    update
    update

--- customer/include/home-header.php
+++ customer/include/home-header.php
@@ -6,38 +6,60 @@
     <img class="fifth-lec" src="assets/img/vet-dog.png" alt="img">
 
     <div class="sticky-top">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-        <div class="logo p-3 d-flex align-items-center">
-            <img class="logo-clinic" src="assets/img/logo.png" alt="Logo" class="logosec">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background: none;">
+            <div class="container-fluid align-items-center d-flex">
+                <!-- Logo -->
+                <div class="logo p-3 d-flex align-items-center">
+                    <img class="logo-clinic" src="assets/img/logo.png" alt="Logo" class="logosec img-fluid">
+                    <div class="logo-text px-3">PAWSsible Solutions <br> Veterinary Clinic</div>
+                </div>
 
-            <div class="logo-text px-3">PAWSsible Solutions <br>
-                Veterinary Clinic</div>
-        </div>
-        <div class="collapse navbar-collapse " id="navbarSupportedContent">
-            <div class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <div class="navbar-links">
-                <a href="home.php">Home</a>
-                <a href="services.php">Services</a> 
-                <a href="aboutus.php">About Us</a>
-                <a href="booking.php">Booking</a>
-                <div class="dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-bell"></i> Account
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="accountDropdown">
-                        <li><a class="dropdown-item" href="customer-profile.php">My Profile</a></li>
-                        <li><a class="dropdown-item" href="appointment.php">Appointments</a></li>
-                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                <!-- Navbar toggler button -->
+                <div class=" d-flex justify-content-end align-items-center">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+                <!-- Navbar links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-links navbar-nav justify-content-center align-items-center">
+                        <li class="navbar-links">
+                            <a href="home.php">Home</a>
+                        </li>
+                        <li class="navbar-links">
+                            <a href="services.php">Services</a>
+                        </li>
+                        <li class="navbar-links">
+                            <a href="aboutus.php">About Us</a>
+                        </li>
+                        <li class="navbar-links">
+                            <a href="booking.php">Booking</a>
+                        </li>
+                        <li class="navbar-links dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa fa-bell"></i> Account
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="accountDropdown">
+                                <li><a class="dropdown-item" href="customer-profile.php">My Profile</a></li>
+                                <li><a class="dropdown-item" href="appointment.php">Appointments</a></li>
+                                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </div>
-            </div>
-        </div>
-        </div>
-    </nav>
+        </nav>
     </div>
 </header>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var navbarCollapse = document.getElementById("navbarSupportedContent");
+        var navbarToggler = document.querySelector(".navbar-toggler");
+
+        navbarToggler.addEventListener("click", function() {
+            navbarCollapse.classList.toggle("bg-color");
+        });
+    });
+</script>
