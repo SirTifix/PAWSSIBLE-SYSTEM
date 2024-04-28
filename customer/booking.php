1bdc3b01b4cbae737e1e6a4192f2937f7fd84012
cyphrus <rafaelcastelo2003@gmail.com>
2024-04-29 04:27:23 +0800

    changes

--- customer/booking.php
+++ customer/booking.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user'] != 'customer'){
 <!DOCTYPE html>
 <html lang="en">
 <?php
-$title = 'Pawssible Solutions Veterinary';
+$title = 'Booking';
 require_once ('./tools/functions.php');
 ?>
 
