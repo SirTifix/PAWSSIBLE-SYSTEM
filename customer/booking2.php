af034b36bb1914f49179d1f9d4b1d23b743b3213
SirTifix <saludoraf@gmail.com>
2024-04-29 00:19:14 +0800

    booking responsiveness

--- customer/booking2.php
+++ customer/booking2.php
@@ -16,169 +16,169 @@
 
 </head>
 <body>
-<?php
+<?php 
 session_start();
 ?>
-<h2 class="mb-4">Pet Information Form <?php echo $_SESSION['noofpets'] . ' pets'; ?> </h2>
-<?php for ($i = 0; $i < $_SESSION['noofpets']; $i++) { ?>
-        <div class="pet-info-form background-color-container">
+<h2 class="mb-4">Pet Information Form <?php echo $_SESSION['noofpets'].' pets';?> </h2>
+<?php for ($i = 0; $i < $_SESSION['noofpets']; $i++) {?>
+<div class="pet-info-form background-color-container">
     
-            <form action="submit-booking2.php" class="needs-validation" method="post" novalidate> 
-                <div class="form-row">
-                <input type="hidden" name="pet_index[]" value="<?php echo $i; ?>">
-                <input type="hidden" name="bookingID[]" value="<?php echo isset($_GET['bookingID']) ? $_GET['bookingID'] : ''; ?>">
-                    <div class="form-group col-sm-2  background-color">
-                        <label for="petName">
-                            <h5>Pet Name</h5>
-                        </label>
-                        <input type="text" class="form-control" id="petname_<?php echo $i; ?>" name="petName[]" 
-                            placeholder="Enter pet name" required/>
-                    </div>
-                    <div class="form-group col-sm-2 background-color">
-                        <label for="petType_<?php echo $i; ?>">
-                            <h5>Pet Type</h5>
-                        </label>
-                        <input type="text" class="form-control" id="pettype_<?php echo $i; ?>" name="petType[]"
-                            placeholder="Enter pet type" required/>
-                    </div>
-                    <div class="form-group col-sm-2 background-color">
-                        <label for="sex_<?php echo $i; ?>">
-                            <h5>Sex</h5>
-                        </label>
-                        <select class="form-control" id="services_<?php echo $i; ?>" name="sex[]" required>
-                            <option value="">Choose...</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                    </div>
+    <form action="submit-booking2.php" method="post" >
+        <div class="form-row">
+        <input type="hidden" name="pet_index[]" value="<?php echo $i; ?>">
+        <input type="hidden" name="bookingID[]" value="<?php echo isset($_GET['bookingID']) ? $_GET['bookingID'] : ''; ?>">
+            <div class="form-group col-sm-2  background-color">
+                <label for="petName">
+                    <h5>Pet Name</h5>
+                </label>
+                <input type="text" class="form-control" id="petname_<?php echo $i; ?>" name="petName[]" 
+                    placeholder="Enter pet name" />
+            </div>
+            <div class="form-group col-sm-2 background-color">
+                <label for="petType_<?php echo $i; ?>">
+                    <h5>Pet Type</h5>
+                </label>
+                <input type="text" class="form-control" id="pettype_<?php echo $i; ?>" name="petType[]"
+                    placeholder="Enter pet type" />
+            </div>
+            <div class="form-group col-sm-2 background-color">
+                <label for="sex_<?php echo $i; ?>">
+                    <h5>Sex</h5>
+                </label>
+                <select class="form-control" id="services_<?php echo $i; ?>" name="sex[]">
+                    <option value="">Choose...</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            </div>
 
-                    <div class="form-group col-sm-2">
-                        <label for="concerns_<?php echo $i; ?>">
-                            <h5>Concerns</h5>
-                        </label>
-                        <textarea style="height: 200px;" class="form-concerns" name="concerns[]"
-                            id="concerns_<?php echo $i; ?>"></textarea>
-                    </div>
-                </div>
+            <div class="form-group col-sm-2">
+                <label for="concerns_<?php echo $i; ?>">
+                    <h5>Concerns</h5>
+                </label>
+                <textarea class="form-concerns" name="concerns[]"
+                    id="concerns_<?php echo $i; ?>"></textarea>
+            </div>
+        </div>
 
-                <div class="form-row">
-                    <div class="form-group col-sm-2 background-color">
-                        <label for="breed_<?php echo $i; ?>">
-                            <h5>Breed</h5>
-                        </label>
-                        <input type="text" class="form-control" id="breed_<?php echo $i; ?>" name="petBreed[]"
-                            placeholder="Enter breed" required/>
-                    </div>
-                    <div class="form-group col-sm-2 background-color">
-                        <label for="services_<?php echo $i; ?>">
-                            <h5>Select Services</h5>
-                        </label>
-                        <select class="form-control" id="services_<?php echo $i; ?>" name="services[]" required>
-                            <option value="">Choose...</option>
-                            <?php
-                            require_once '../classes/service.class.php';
-                            $service = new Service();
-                            $services = $service->show();
-                            foreach ($services as $service) {
-                                echo '<option value="' . $service['serviceID'] . '">' . $service['serviceName'] . ' - &#8369;' . $service['servicePrice'] . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
+        <div class="form-row">
+            <div class="form-group col-sm-2 background-color">
+                <label for="breed_<?php echo $i; ?>">
+                    <h5>Breed</h5>
+                </label>
+                <input type="text" class="form-control" id="breed_<?php echo $i; ?>" name="petBreed[]"
+                    placeholder="Enter breed" />
+            </div>
+            <div class="form-group col-sm-2 background-color">
+                <label for="services_<?php echo $i; ?>">
+                    <h5>Select Services</h5>
+                </label>
+                <select class="form-control" id="services_<?php echo $i; ?>" name="services[]">
+                    <option value="">Choose...</option>
+                    <?php
+                    require_once '../classes/service.class.php';
+                    $service = new Service();
+                    $services = $service->show();
+                    foreach ($services as $service) {
+                        echo '<option value="' . $service['serviceID'] . '">' . $service['serviceName'] . ' - &#8369;' . $service['servicePrice'] . '</option>';
+                    }
+                    ?>
+                </select>
+            </div>
 
-                    <div class="form-group col-sm-2 background-color">
-                        <label for="vet_<?php echo $i; ?>">
-                            <h5>Select vet</h5>
-                        </label>
-                        <select class="form-control" id="vet_<?php echo $i; ?>" name="vet[]" required>
-                            <option value="">Choose...</option>
-                            <?php
-                            require_once '../classes/veterinarian.class.php';
-                            $vet = new Veterinarian();
-                            $vets = $vet->show();
-                            foreach ($vets as $vet) {
-                                echo '<option value="' . $vet['vetID'] . '">Dr. ' . $vet['vetFirstname'] . ' ' . $vet['vetLastname'] . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
+            <div class="form-group col-sm-2 background-color">
+                <label for="vet_<?php echo $i; ?>">
+                    <h5>Select vet</h5>
+                </label>
+                <select class="form-control" id="vet_<?php echo $i; ?>" name="vet[]">
+                    <option value="">Choose...</option>
+                    <?php
+                    require_once '../classes/veterinarian.class.php';
+                    $vet = new Veterinarian();
+                    $vets = $vet->show();
+                    foreach ($vets as $vet) {
+                        echo '<option value="' . $vet['vetID'] . '">Dr. ' . $vet['vetFirstname'] . ' ' . $vet['vetLastname'] . '</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
 
-                <div class="form-row">
-                    <div class="form-group col-sm-2 background-color">
-                        <label for="birthdate_<?php echo $i; ?>">
-                            <h5>BirthDate</h5>
-                        </label>
-                        <input type="date" class="form-control" id="birthdate_<?php echo $i; ?>" name="petBirthDate[]" required/>
-                    </div>
-                </div>
+        <div class="form-row">
+            <div class="form-group col-sm-2 background-color">
+                <label for="birthdate_<?php echo $i; ?>">
+                    <h5>BirthDate</h5>
+                </label>
+                <input type="date" class="form-control" id="birthdate_<?php echo $i; ?>" name="petBirthDate[]" />
+            </div>
+        </div>
 
-                <div class="select-ex-pet">
-                    <button type="button" class="btn btn-primary select-button" data-bs-toggle="modal" data-bs-target="#profileModal"
-                        style="background-color: #6075d1; float: right;">
-                        Select Existing Pet
-                    </button>
+        <div class="select-ex-pet">
+            <button type="button" class="btn btn-primary select-button" data-bs-toggle="modal" data-bs-target="#profileModal"
+                style="background-color: #6075d1; float: right;">
+                Select Existing Pet
+            </button>
 
-                    <!-- Profile Modal -->
-                    <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel"
-                        aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-lg">
-                            <div class="modal-content">
-                                <div class="modal-body">
+            <!-- Profile Modal -->
+            <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-body">
 
 
-                                    <div class="content p-4 w-100">
-                                        <table class="table table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th>Pet No.</th>
-                                                    <th>Pet Name</th>
-                                                    <th>Species</th>
-                                                    <th>Action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>4412</td>
-                                                    <td>Max</td>
-                                                    <td>Dog</td>
-                                                    <td> <button type="button"
-                                                            class="Select-button btn btn-primary aria-expanded=" false">
-                                                            Select
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>4413</td>
-                                                    <td>Max</td>
-                                                    <td>Dog</td>
-                                                    <td> <button type="button"
-                                                            class="Select-button btn btn-primary aria-expanded=" false">
-                                                            Select
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>4414</td>
-                                                    <td>Kebies</td>
-                                                    <td>Cat</td>
-                                                    <td> <button type="button"
-                                                            class="Select-button btn btn-primary aria-expanded=" false">
-                                                            Select
-                                                        </button>
-                                                    </td>
+                            <div class="content p-4 w-100">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Pet No.</th>
+                                            <th>Pet Name</th>
+                                            <th>Species</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>4412</td>
+                                            <td>Max</td>
+                                            <td>Dog</td>
+                                            <td> <button type="button"
+                                                    class="Select-button btn btn-primary aria-expanded=" false">
+                                                    Select
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>4413</td>
+                                            <td>Max</td>
+                                            <td>Dog</td>
+                                            <td> <button type="button"
+                                                    class="Select-button btn btn-primary aria-expanded=" false">
+                                                    Select
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>4414</td>
+                                            <td>Kebies</td>
+                                            <td>Cat</td>
+                                            <td> <button type="button"
+                                                    class="Select-button btn btn-primary aria-expanded=" false">
+                                                    Select
+                                                </button>
+                                            </td>
 
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
+
                     </div>
                 </div>
+            </div>
         </div>
+</div>
 
 <?php } ?>
 
@@ -187,9 +187,21 @@ session_start();
             Finish Booking
     </button>
 </div>
-</form>
 
-<script src="./assets/script/validation.js"></script>
+<script src="./assets/script/fetch.js"></script>
+ <script>
+    $(document).ready(function () {
+      $('#anotherModal').on('show.bs.modal', function (e) {
+        $('.modal').modal('hide');
+      });
+    });
+
+
+    document.getElementById('finishBookingBtn').addEventListener('click', function () {
+      location.reload();
+    });
+  </script>
+
 </body>
 </html>
 
