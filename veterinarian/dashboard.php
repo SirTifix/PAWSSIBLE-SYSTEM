e25affd3fbdb30b92652dfb8f3a046b38240d49b
PurpleMariposa <salazarkrystel405@gmail.com>
2024-05-05 18:36:54 +0800

    new updates status for veterinarian
    dashboard

--- veterinarian/dashboard.php
+++ veterinarian/dashboard.php
@@ -1,76 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    $title = 'Dashboard';
-    require_once('./include/vet-head.php');
-    require_once('../classes/veterinarian.class.php');
+$title = 'Dashboard';
+require_once('./include/vet-head.php');
+require_once('../classes/veterinarian.class.php');
 
-    $vetRecordClass = new Veterinarian;
-    $vetID = $vetRecordClass->vetID;
+$vetRecordClass = new Veterinarian;
+$vetID = $vetRecordClass->vetID;
 
-    $vetCount = $vetRecordClass->countVet($vetID);
+$vetCount = $vetRecordClass->countVet($vetID);
 ?>
 
 <body>
   <?php
-        require_once('./include/vet-header.php')
-    ?>
-   <?php
-        require_once('./include/vet-sidepanel.php')
-    ?>
-     <section class="dashboard-con mb-5 ">
-        <div class="box">
-            <div class="dashboard-head mb-5">
-              <p>Welcome to Dashboard!</p>
-            </div>
-
-            <div class="dashboard-container"> 
-
-                <div class="dashboard-box py-1 px-3">
-                    <p class="my-1">Customer Handled: </p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <i class="dashboard-icon fa-solid fa-users m-2" aria-hidden="true"></i>
-                      <h1 class="m-3"><?php echo $vetCount; ?></h1>
-                    </div>
-                </div> 
-
-                <div class="dashboard-box py-1 px-3">
-                    <p class="my-1">Upcoming Appointments: </p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <i class="dashboard-icon fa-solid  fa-clock  m-2" aria-hidden="true"></i>
-                      <h1 class="m-3">0</h1>
-                    </div>
-                </div> 
-      
-            </div>
-        </div>
-    </section>
-
-    <section class="customer-table-con">
-      <div class="cust-table-wrapper">
-        <p class="cust-text">Recent Appointments</p>
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Book No.</th>
-              <th scope="col">Name</th>
-              <th scope="col">Date</th>
-              <th scope="col">Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row"></th>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+  require_once('./include/vet-header.php')
+  ?>
+  <?php
+  require_once('./include/vet-sidepanel.php')
+  ?>
+  <section class="dashboard-con mb-5 ">
+    <div class="box">
+      <div class="dashboard-head mb-5">
+        <p>Welcome to Dashboard!</p>
       </div>
-    </section>
-    </main>
-    
-</body>
-</php>
 
+      <div class="status-vet-con d-flex align-items-center">
+        <div class="col-6 text-start">
+          <div class="status-border mx-5">Status: Active status</div>
+        </div>
+        <div class="col-6 text-end">
+          <button id="availabilityButton" class="btn btn-success mx-5" data-bs-toggle="modal" data-bs-target="#availabilityModal">Change Status</button>
+        </div>
+      </div>
+
+      <!-- Modal Section -->
+      <div class="modal fade" id="availabilityModal" tabindex="-1" role="dialog" aria-labelledby="availabilityModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content" style="max-width: 400px;">
+            <button type="button" data-bs-dismiss="modal" aria-label="Close" style="display:flex; justify-content: flex-end; font-size: 1.5em;">
+              <i class="fa-solid fa-circle-xmark"></i></button>
+            <h5 class="modal-title text-center" id="availabilityModalLabel">Change Status</h5>
+            <div class="modal-body d-flex align-items-center justify-content-between">
+              <button type="button" class="btn btn-success">Available</button>
+              <button type="button" class="btn btn-secondary">Unavailable</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="dashboard-container">
+        <div class="dashboard-box py-1 px-3">
+          <p class="my-1">Customer Handled: </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <i class="dashboard-icon fa-solid fa-users m-2" aria-hidden="true"></i>
+            <h1 class="m-3"><?php echo $vetCount; ?></h1>
+          </div>
+        </div>
+
+        <div class="dashboard-box py-1 px-3">
+          <p class="my-1">Upcoming Appointments: </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <i class="dashboard-icon fa-solid  fa-clock  m-2" aria-hidden="true"></i>
+            <h1 class="m-3">0</h1>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="customer-table-con">
+    <div class="cust-table-wrapper">
+      <p class="cust-text">Recent Appointments</p>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Book No.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Date</th>
+            <th scope="col">Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
--- veterinarian/include/vet-head.php
+++ veterinarian/include/vet-head.php
@@ -4,6 +4,7 @@
     <title><?php echo $title ?></title>
     <link rel="website icon" type="png" href="./assets/img/Logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />  
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="vendor/bootstrap-5.0.2/css/bootstrap.min.css">
